6f2bd85725f2a0c7d196ecd546b08def074586fb
NotSoToxic <sarthak.joshi52@gmail.com>
2024-03-09 00:11:01 +0530

    update{     contact.jsx }

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -95,6 +95,33 @@ export const Contact = (props) => {
               </form>
             </div>
           </div>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Address
+                </span>
+                {props.data ? props.data.address : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Phone
+                </span>{" "}
+                {props.data ? props.data.phone : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
