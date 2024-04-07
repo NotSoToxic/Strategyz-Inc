832733b318984226190c123e62eb5ecef8fbf159
NotSoToxic <sarthak.joshi52@gmail.com>
2024-04-07 13:49:33 +0530

    update{     data.json     about.jsx     strategyzinfo.jsx     info.jsx     style.css
        routing removed for about subsection
    }

--- src/components/sub-components/about/strategyzinfo.jsx
+++ src/components/sub-components/about/strategyzinfo.jsx
@@ -1,11 +1,66 @@
 import React from "react";
+import 'react-multi-carousel/lib/styles.css';
 
 export const Strategyzinfo = (props) => {
     return (
         <div id="strategyzinfo">
-            <h1>
-                Coming Soon...
-            </h1>
+             <div className="container">
+             <div className="row">
+             <div className="col-xs-12 col-md-6">
+                {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            </div>
+            {/* <div className="col-xs-12 col-md-6"> */}
+            <div className="info-text1">
+                {/* unable to create data paragraph in data.json in this */}
+              {/* <p>{props.data ? props.data.paragraph : "loading..."}</p> */}
+              <p>Veniam irure in consectetur amet cupidatat est tempor mollit anim reprehenderit anim. Ex fugiat ullamco tempor irure qui proident occaecat veniam dolor proident nulla laboris laboris. Ipsum ullamco sint exercitation nisi tempor excepteur mollit laboris eiusmod dolore. Esse esse dolore esse do minim ea labore est ut deserunt sint. Mollit fugiat qui commodo cillum eiusmod excepteur proident.</p>
+            </div>
+            
+          
+        </div>
+        </div>
+
+        <div className="container">
+             <div className="row">
+             <div className="col-xs-12 col-md-6">
+                        {/* Side Navbar */}
+                        <nav className="side-navbar">
+                            <ul>
+                                <li><a href="#section1">Section 1</a></li>
+                                <li><a href="#section2">Section 2</a></li>
+                                <li><a href="#section3">Section 3</a></li>
+                                <li><a href="#section4">Section 3</a></li>
+                                {/* Add more items as needed */}
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="side-nav-mar">
+                        <div className="info-text">
+                            {/* <div className="container-sec"> */}
+                            <section id="section1">
+                                <h2>Section 1</h2>
+                                <p>Ex fugiat ullamco tempor irure qui proident occaecat veniam dolor proident nulla laboris laboris. Ipsum ullamco sint exercitation nisi tempor excepteur mollit laboris eiusmod dolore. Esse esse dolore esse do minim ea labore est ut deserunt sint. Mollit fugiat qui commodo cillum eiusmod excepteur proident.</p>
+                            </section>
+                            {/* <section id="section2">
+                                <h2>Section 2</h2>
+                                <p>Ex fugiat ullamco tempor irure qui proident occaecat veniam dolor proident nulla laboris laboris. Ipsum ullamco sint exercitation nisi tempor excepteur mollit laboris eiusmod dolore. Esse esse dolore esse do minim ea labore est ut deserunt sint. Mollit fugiat qui commodo cillum eiusmod excepteur proident.</p>
+                            </section>
+                            <section id="section3">
+                                <h2>Section 3</h2>
+                                <p>Ex fugiat ullamco tempor irure qui proident occaecat veniam dolor proident nulla laboris laboris. Ipsum ullamco sint exercitation nisi tempor excepteur mollit laboris eiusmod dolore. Esse esse dolore esse do minim ea labore est ut deserunt sint. Mollit fugiat qui commodo cillum eiusmod excepteur proident.</p>
+                            </section> */}
+                            {/* Add more sections as needed */}
+                        {/* </div> */}
+                        </div>
+            
+             </div>
+             </div>
+                </div>
+                
+
+
+        {/* Info div close */}
         </div>
     );
 };
