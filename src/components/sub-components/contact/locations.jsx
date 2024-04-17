313d9b2d2fd8598448c9f86018a9e399f6bfb2b9
Devashish jindal <mohijindal28@gmail.com>
2024-04-17 19:11:27 +0530

    updates{     location.jsx } code formatting

--- src/components/sub-components/contact/locations.jsx
+++ src/components/sub-components/contact/locations.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-const firstLocation =
-  "STRATEGYZ Technologies PVT. LTD. We Work Institutional Area, Sector 44, Gurgaon, Haryana, India - 122002";
-const secondLocation =
-  "STRATEGYZ Technologies PVT. LTD. Bannerghatta Road, Bengaluru, Karnataka, India - 560076";
-const iframeSrcAddress1 =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.544486188889!2d77.0646029!3d28.4560152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6af56b5a1202865b%3A0xdf2a55d5213c97ff!2sStrategyz+Inc.!5e0!3m2!1sen!2sin!4v1645561134858!5m2!1sen!2sin";
-const iframeSrcAddress2 =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.544486188889!2d77.0646029!3d28.4560152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6af56b5a1202865b%3A0xdf2a55d5213c97ff!2sStrategyz+Inc.!5e0!3m2!1sen!2sin!4v1645561134858!5m2!1sen!2sin";
+
+
+
 export const Locations = (props) => {
+  const firstLocation =
+  "STRATEGYZ Technologies PVT. LTD. We Work Institutional Area, Sector 44, Gurgaon, Haryana, India - 122002";
+  const secondLocation =
+    "STRATEGYZ Technologies PVT. LTD. Bannerghatta Road, Bengaluru, Karnataka, India - 560076";
+  const iframeSrcAddress1 =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.544486188889!2d77.0646029!3d28.4560152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6af56b5a1202865b%3A0xdf2a55d5213c97ff!2sStrategyz+Inc.!5e0!3m2!1sen!2sin!4v1645561134858!5m2!1sen!2sin";
+  const iframeSrcAddress2 =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.544486188889!2d77.0646029!3d28.4560152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6af56b5a1202865b%3A0xdf2a55d5213c97ff!2sStrategyz+Inc.!5e0!3m2!1sen!2sin!4v1645561134858!5m2!1sen!2sin";
+
+    
   return (
     <>
       <div id="location">
@@ -47,7 +52,7 @@ export const Locations = (props) => {
           </div>
           
           <div id="location-form">
-          <div className="">
+          <div>
                 <h2>Contact Us</h2>
           </div>
               <form name="" validate >
