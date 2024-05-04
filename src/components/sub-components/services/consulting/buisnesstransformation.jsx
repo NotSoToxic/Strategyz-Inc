c9469715e9be6e2ce707fd91710ce50b1e92aeec
Atharva Jaurkar <atharvajaurkar.2003@gmail.com>
2024-05-04 19:19:39 +0530

    section 1 added

--- src/components/sub-components/services/consulting/buisnesstransformation.jsx
+++ src/components/sub-components/services/consulting/buisnesstransformation.jsx
@@ -3,9 +3,39 @@ import React from "react";
 export const Buisnesstransformation = (props) => {
     return (
         <div id="buisnesstransformation">
-            <h1>
-                Coming Soon...
-            </h1>
+        
+        <section id="empowerBuisness">
+
+            <h2 className="display-1 " id="b2">Empower Your Business with IT Consulting</h2>
+            <h1 className="display-1" id="headding">Maximize Effieciency and Innovation </h1>
+
+            <div className="sec1buttons">
+            <div className="getStartedB">
+            <button type="button" class="btn btn-lg" id="buisnessButton">Get Started</button>
+            </div>
+            <div className="learnMoreB">
+            <button type="button" class="btn btn-lg" id="buisnessButton">Learn More</button>
+            </div>
+            </div>
+
+            
+
+        </section>
+        
+        <section id="features">
+      <h2>Our Key Consulting Features</h2>
+      <p>Empowering businesses through strategic transformation</p>
+      <ul>
+        <li>
+          <h3>Strategic Planning</h3>
+          <p>Develop customized strategies to drive business growth</p>
+        </li>
+        <li>
+          <h3>Change Management</h3>
+          <p>Implement effective change management processes for smooth transitions</p>
+        </li>
+      </ul>
+    </section>
         </div>
     );
 };
