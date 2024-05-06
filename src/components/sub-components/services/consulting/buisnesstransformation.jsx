a9c76e2f7329c36ae0869f6c0cb34afa27f08d99
Atharva Jaurkar <atharvajaurkar.2003@gmail.com>
2024-05-06 14:26:03 +0530

    work in progress

--- src/components/sub-components/services/consulting/buisnesstransformation.jsx
+++ src/components/sub-components/services/consulting/buisnesstransformation.jsx
@@ -6,8 +6,8 @@ export const Buisnesstransformation = (props) => {
         
         <section id="empowerBuisness">
 
-            <h2 className="display-1 " id="b2">Empower Your Business with IT Consulting</h2>
-            <h1 className="display-1" id="headding">Maximize Effieciency and Innovation </h1>
+            <h2 className="display-1 " id="b2">Empower Your Buisness with IT Consulting</h2>
+            <h1 className="display-1" id="headding">Maximize Efficency and Innovation </h1>
 
             <div className="sec1buttons">
             <div className="getStartedB">
@@ -25,7 +25,27 @@ export const Buisnesstransformation = (props) => {
         <section id="features">
       <h2>Our Key Consulting Features</h2>
       <p>Empowering businesses through strategic transformation</p>
-      <ul>
+      
+      
+      <div className="featureSection">
+      
+      <div class id="strategicPlanning">
+        <h3>Strategic Planning</h3>
+      </div>
+      
+      <div id="changeManagement">
+        <h3>Change Management</h3>
+      </div>
+      <div id="offered">
+        <h3>Services Offered</h3>
+      </div>
+      <div id="whyChoose">
+        <h3>Why choose us ?</h3>
+      </div>
+      
+      </div>
+      
+      {/* <ul>
         <li>
           <h3>Strategic Planning</h3>
           <p>Develop customized strategies to drive business growth</p>
@@ -34,7 +54,7 @@ export const Buisnesstransformation = (props) => {
           <h3>Change Management</h3>
           <p>Implement effective change management processes for smooth transitions</p>
         </li>
-      </ul>
+      </ul> */}
     </section>
         </div>
     );
