e2c3f61a003b309f4485da1505dad1defaba6d29
Atharva Jaurkar <atharvajaurkar.2003@gmail.com>
2024-05-14 10:22:23 +0530

    buisnessTransformation switched to staffAugmentation, style also updated

--- src/components/sub-components/services/consulting/augmentation.jsx
+++ src/components/sub-components/services/consulting/augmentation.jsx
@@ -2,10 +2,85 @@ import React from "react";
 
 export const Augmentation = (props) => {
     return (
-        <div id="augmentation">
-            <h1>
-                Coming Soon...
-            </h1>
+        <div id="staffAugmentation">
+        
+        <section id="empowerStaff">
+
+            <h2 className="display-1 " id="b2">Transform Your Buisness with our IT Consulting</h2>
+            <h1 className="display-1" id="headding">Maximize Efficency and Innovation </h1>
+
+            <div className="sec1buttons">
+            <div className="getStartedB">
+            <button type="button" class="btn btn-lg" id="staffButton" href="#features">
+              <a href="#features">Learn More</a> 
+            </button>
+            </div>
+            <div className="learnMoreB">
+            <button type="button" class="btn btn-lg" id="buisnessButton" >
+                {/* redirected to info page for form filling */}
+                 <a href="/info">Get Started</a>  
+            </button>
+            </div>
+            </div>
+
+            
+
+        </section>
+        
+        <section id="features">
+      <h2 id="fH">Our Key Consulting Features</h2>
+      <h3 id="fH">Empowering businesses through strategic transformation</h3>
+      
+      
+      <div id="featuresSection" className="featureSection container">
+      
+      <div id="firstRow" className="row">
+      <div class id="strategicPlanning" className=" col-md-6" >
+        <h3>Strategic Planning</h3>
+        <p className=""> Develop customized strategies to drive buisness growth </p>
+      </div> 
+
+      <div id="changeManagement" className="col-md-6">
+        <h3>Change Management</h3>
+        <p>Implement effective change management processes for smooth transitions</p>
+      </div>
+      </div>
+
+      <div id="secondRow" className="row">
+      <div id="offered" className="col-md-6" >
+        <h3>Services Offered</h3>
+        <ul>
+          <li>Contract Staffing</li>
+          <li>Permanent Staffing</li>
+          <li>Contact-to-hire Services</li>
+          <li> Executive search and placement</li>
+        </ul>
+      </div>
+
+      <div id="whyChoose" className="col-md-6">
+        <h3>Why choose us ?</h3>
+        <ul>
+          <li>Experienced Team of recruiters and HR</li>
+          <li>Proven track record of sucessful placements</li>
+          <li>Customized solutions tailored to client needs </li>
+          <li>Extensive candidate database and network</li>
+        </ul>
+      </div>
+      </div>
+      
+      </div>
+      
+      {/* <ul>
+        <li>
+          <h3>Strategic Planning</h3>
+          <p>Develop customized strategies to drive business growth</p>
+        </li>
+        <li>
+          <h3>Change Management</h3>
+          <p>Implement effective change management processes for smooth transitions</p>
+        </li>
+      </ul> */}
+    </section>
         </div>
     );
 };
--- src/components/sub-components/services/consulting/buisnesstransformation.jsx
+++ src/components/sub-components/services/consulting/buisnesstransformation.jsx
@@ -2,82 +2,90 @@ import React from "react";
 
 export const Buisnesstransformation = (props) => {
     return (
-        <div id="buisnesstransformation">
+      <div>
+        <h1>
+          Comming Soon
+        </h1>
+      </div>
+    );
+
+    
+    //     <div id="buisnesstransformation">
         
-        <section id="empowerBuisness">
+    //     <section id="empowerBuisness">
 
-            <h2 className="display-1 " id="b2">Transform Your Buisness with our IT Consulting</h2>
-            <h1 className="display-1" id="headding">Maximize Efficency and Innovation </h1>
+    //         <h2 className="display-1 " id="b2">Transform Your Buisness with our IT Consulting</h2>
+    //         <h1 className="display-1" id="headding">Maximize Efficency and Innovation </h1>
 
-            <div className="sec1buttons">
-            <div className="getStartedB">
-            <button type="button" class="btn btn-lg" id="buisnessButton" href="#features">
-              <a href="#features">Get Started</a> 
-            </button>
-            </div>
-            <div className="learnMoreB">
-            <button type="button" class="btn btn-lg" id="buisnessButton">Learn More</button>
-            </div>
-            </div>
+    //         <div className="sec1buttons">
+    //         <div className="getStartedB">
+    //         <button type="button" class="btn btn-lg" id="buisnessButton" href="#features">
+    //           <a href="#features">Get Started</a> 
+    //         </button>
+    //         </div>
+    //         <div className="learnMoreB">
+    //         <button type="button" class="btn btn-lg" id="buisnessButton">Learn More</button>
+    //         </div>
+    //         </div>
 
             
 
-        </section>
+    //     </section>
         
-        <section id="features">
-      <h2 id="fH">Our Key Consulting Features</h2>
-      <h3 id="fH">Empowering businesses through strategic transformation</h3>
+    //     <section id="features">
+    //   <h2 id="fH">Our Key Consulting Features</h2>
+    //   <h3 id="fH">Empowering businesses through strategic transformation</h3>
       
       
-      <div id="featuresSection" className="featureSection container">
+    //   <div id="featuresSection" className="featureSection container">
       
-      <div id="firstRow" className="row">
-      <div class id="strategicPlanning" className=" col-md-6" >
-        <h3>Strategic Planning</h3>
-        <p className=""> Develop customized strategies to drive buisness growth </p>
-      </div> 
+    //   <div id="firstRow" className="row">
+    //   <div class id="strategicPlanning" className=" col-md-6" >
+    //     <h3>Strategic Planning</h3>
+    //     <p className=""> Develop customized strategies to drive buisness growth </p>
+    //   </div> 
 
-      <div id="changeManagement" className="col-md-6">
-        <h3>Change Management</h3>
-        <p>Implement effective change management processes for smooth transitions</p>
-      </div>
-      </div>
+    //   <div id="changeManagement" className="col-md-6">
+    //     <h3>Change Management</h3>
+    //     <p>Implement effective change management processes for smooth transitions</p>
+    //   </div>
+    //   </div>
 
-      <div id="secondRow" className="row">
-      <div id="offered" className="col-md-6" >
-        <h3>Services Offered</h3>
-        <ul>
-          <li>Contact Staffing</li>
-          <li>Permanent Staffing</li>
-          <li>Contact-to-hire Services</li>
-          <li> Executive search and placement</li>
-        </ul>
-      </div>
+    //   <div id="secondRow" className="row">
+    //   <div id="offered" className="col-md-6" >
+    //     <h3>Services Offered</h3>
+    //     <ul>
+    //       <li>Contact Staffing</li>
+    //       <li>Permanent Staffing</li>
+    //       <li>Contact-to-hire Services</li>
+    //       <li> Executive search and placement</li>
+    //     </ul>
+    //   </div>
 
-      <div id="whyChoose" className="col-md-6">
-        <h3>Why choose us ?</h3>
-        <ul>
-          <li>Experienced Team of recruiters and HR</li>
-          <li>Proven track record of sucessful placements</li>
-          <li>Customized solutions tailored to client needs </li>
-          <li>Extensive candidate database and network</li>
-        </ul>
-      </div>
-      </div>
+    //   <div id="whyChoose" className="col-md-6">
+    //     <h3>Why choose us ?</h3>
+    //     <ul>
+    //       <li>Experienced Team of recruiters and HR</li>
+    //       <li>Proven track record of sucessful placements</li>
+    //       <li>Customized solutions tailored to client needs </li>
+    //       <li>Extensive candidate database and network</li>
+    //     </ul>
+    //   </div>
+    //   </div>
       
-      </div>
+    //   </div>
       
-      {/* <ul>
-        <li>
-          <h3>Strategic Planning</h3>
-          <p>Develop customized strategies to drive business growth</p>
-        </li>
-        <li>
-          <h3>Change Management</h3>
-          <p>Implement effective change management processes for smooth transitions</p>
-        </li>
-      </ul> */}
-    </section>
-        </div>
-    );
+    //   {/* <ul>
+    //     <li>
+    //       <h3>Strategic Planning</h3>
+    //       <p>Develop customized strategies to drive business growth</p>
+    //     </li>
+    //     <li>
+    //       <h3>Change Management</h3>
+    //       <p>Implement effective change management processes for smooth transitions</p>
+    //     </li>
+    //   </ul> */}
+    // </section>
+    //     </div>
+    // );
 };
