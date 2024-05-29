c023f82b6f22b66ab8c638b434909c7d60c7b7b4
Devashish jindal <mohijindal28@gmail.com>
2024-05-29 13:14:53 +0530

    style: Update background images and colors for landing page

--- src/components/landingpage/about.jsx
+++ src/components/landingpage/about.jsx
@@ -33,7 +33,7 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/about-img.png" className="img-responsive" alt="" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
