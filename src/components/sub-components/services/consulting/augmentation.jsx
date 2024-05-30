cf3b068ef4743feea629a9610cab5448c11bcac2
iamnotBugs <balrajdhakar2002@gmail.com>
2024-05-30 22:12:21 +0530

    updated augmentation page and data.json file

--- src/components/sub-components/services/consulting/augmentation.jsx
+++ src/components/sub-components/services/consulting/augmentation.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-
+import augumentData from "../../../../data/data.json";
+import { Link } from "react-router-dom";
 
 export const Augmentation = (props) => {
   return (
-    <div id="augmentation">
-      <div className="intro-ag">
+    <div id="buisnesstransformation">
+      <div className="buisness-bg">
         <div className="augmentation-banner">
           <div className="overlay">
-            <div className="augmentation-banner-text">
+            <div className="buisness-banner-text">
               <h1>HR and talent transformation consulting</h1>
-              <p className="augmentation-banner-content">
-                  Reimagine and modernize HR with AI at the core to deliver bettter business outcomes, and unlock employee and work potential
+              <p className="buisness-banner-content">
+                Reimagine and modernize HR with AI at the core to deliver
+                bettter business outcomes, and unlock employee and work
+                potential
               </p>
             </div>
           </div>
@@ -41,55 +44,57 @@ export const Augmentation = (props) => {
           <section id="overview">
             <h2>Overview</h2>
             <p>
-              “As businesses face multiple challenges: evolving employee expectations, a global labor shortage, and a growing skills gap, HR leaders are being looked to for answers and leadership. HR leaders have a critical role in turning their organization into one that anticipates disruption and moves with agility to compete and win in uncertain times. HR organizations must evolve and adopt new ways of working between running-the-business and driving transformation initiatives. Doing so will enable them to balance strategy and execution.”
+              “As businesses face multiple challenges: evolving employee
+              expectations, a global labor shortage, and a growing skills gap,
+              HR leaders are being looked to for answers and leadership. HR
+              leaders have a critical role in turning their organization into
+              one that anticipates disruption and moves with agility to compete
+              and win in uncertain times. HR organizations must evolve and adopt
+              new ways of working between running-the-business and driving
+              transformation initiatives. Doing so will enable them to balance
+              strategy and execution.”
             </p>
           </section>
 
           <section id="working">
             <h2>Capabilities</h2>
-            <p>
+            {/* <p>
               We bring together all the necessary technology and services to
               help our clients solve their business problems. Bringing together
               a diverse set of voices with new technology, we collaborate
               closely, ideate freely and swiftly apply breakthrough innovations
               that drive big impact.
-            </p>
+            </p> */}
+            <div id="working-navbar-content">
+              <div className="job-container">
+                {augumentData.augument.map((augument) => (
+                  <div key={augumentData.domain} className="buisness-card">
+                    <h4>{augument.title}</h4>
+                    <p>{augument.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </section>
 
           <section id="clients">
             <h2>Next Steps</h2>
-            <div className="flex-container">
-              <div className="box">
-                <img src="img/StrategyzInfo_logo/ibm.png" alt="IBM logo" />
-                <p>IBM</p>
+            <div className="job-container">
+              <div className="buisness-card">
+                <h4>Get Started</h4>
+                <p>
+                  Connect with our diverse group of Strategyz experts that can
+                  help you make your next big move.
+                </p>
+                <Link to="/info">Contact Us</Link>
               </div>
-              <div className="box">
-                <img
-                  src="img/StrategyzInfo_logo/hexawareT logo.jpeg"
-                  alt="Hexaware logo"
-                />
-                <p>Hexaware Technologies</p>
-              </div>
-              <div className="box">
-                <img
-                  src="img/StrategyzInfo_logo/RangT logo.png"
-                  alt="Rang Technologies logo"
-                />
-                <p>Rang Technologies</p>
-              </div>
-              <div className="box">
-                <img
-                  src="img/StrategyzInfo_logo/HarjaiC logo.png"
-                  alt="Harjai Computers logo"
-                />
-                <p>Harjai Computers</p>
-              </div>
-              <div className="box">
-                <img
-                  src="img/StrategyzInfo_logo/fabH logo.png"
-                  alt="Fab Hotels logo"
-                />
-                <p>FabHotels</p>
+              <div className="buisness-card">
+                <h4>Explore career opportunities</h4>
+                <p>
+                  Join our team of dedicated, innovative people who are bringing
+                  positive change to work and the world.
+                </p>
+                <Link to="/careers">Apply now</Link>
               </div>
             </div>
           </section>
