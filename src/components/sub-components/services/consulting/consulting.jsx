177cf3db5ece5ce92de48eda08ab9cf8f7368ac3
Atharva Jaurkar <atharvajaurkar.2003@gmail.com>
2024-05-30 15:33:57 +0530

    consulting overview updated

--- src/components/sub-components/services/consulting/consulting.jsx
+++ src/components/sub-components/services/consulting/consulting.jsx
@@ -19,6 +19,61 @@ export const Consulting = (props) => {
                             </div>
                     
             </div>
+
+            <div className="content-wrapper">
+        <aside className="sidebar">
+          <ul>
+            <li><a href="#overview" className="page-scroll">Overview</a></li>
+            <li><a href="#working" className="page-scroll">Capabilities</a></li> 
+            <li><a href="#clients" className="page-scroll">Next Steps</a></li>
+          </ul>
+        </aside>
+
+        <main>
+          <section id="overview">
+            <h2>Overview</h2>
+            <p>
+              “STRATEGYZ Consulting delivers high-quality and innovative IT solutions to meet our client’s business needs, including hybrid cloud management services with technology solutions that enable their business objectives and accelerate the cloud journey, unleashing the full potential of their technology investments.”
+            </p>
+          </section>
+
+          <section id="working">
+            <h2>Capabilities</h2>
+            <p>
+              We bring together all the necessary technology and services to help our clients solve their business problems. Bringing together a diverse set of voices with new technology, we collaborate closely, ideate freely and swiftly apply breakthrough innovations that drive big impact.
+            </p>
+            
+
+            
+          </section>
+
+          <section id="clients">
+            <h2>Next Steps</h2>
+            <div className="flex-container">
+              <div className="box">
+                <img src="img/StrategyzInfo_logo/ibm.png" alt="IBM logo" />
+                <p>IBM</p>
+              </div>
+              <div className="box">
+                <img src="img/StrategyzInfo_logo/hexawareT logo.jpeg" alt="Hexaware logo" />
+                <p>Hexaware Technologies</p>
+              </div>
+              <div className="box">
+                <img src="img/StrategyzInfo_logo/RangT logo.png" alt="Rang Technologies logo" />
+                <p>Rang Technologies</p>
+              </div>
+              <div className="box">
+                <img src="img/StrategyzInfo_logo/HarjaiC logo.png" alt="Harjai Computers logo" />
+                <p>Harjai Computers</p>
+              </div>
+              <div className="box">
+                <img src="img/StrategyzInfo_logo/fabH logo.png" alt="Fab Hotels logo" />
+                <p>FabHotels</p>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
         </div>
     );
 };
