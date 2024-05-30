7107448ee9a92132fe46a8a154c58a39b4f9aaff
Atharva Jaurkar <atharvajaurkar.2003@gmail.com>
2024-05-30 14:56:41 +0530

    updated consulting page

--- src/components/sub-components/services/consulting/consulting.jsx
+++ src/components/sub-components/services/consulting/consulting.jsx
@@ -3,9 +3,21 @@ import React from "react";
 export const Consulting = (props) => {
     return (
         <div id="consulting">
-            
-            <div className="consulting banner">
-                <img src="" alt="" />
+
+            <div className="intro-bg">
+                <div className="consulting-banner">
+                    <div className="overlay">
+                        
+                                <div className="consulting-banner-text">
+                                        <h1>
+                                        IT Consulting Services
+                                        
+                                        </h1>
+                                        <p>Grow and transform your business by reimagining your corporate strategy and how you work.</p>
+                                    </div>
+                            </div>
+                            </div>
+                    
             </div>
         </div>
     );
