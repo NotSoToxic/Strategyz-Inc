0d78a0091b9a4b20daacf77c2cba0a4111bc238b
Atharva Jaurkar <atharvajaurkar.2003@gmail.com>
2024-05-30 13:58:06 +0530

    big update

--- src/components/sub-components/services/consulting/consulting.jsx
+++ src/components/sub-components/services/consulting/consulting.jsx
@@ -3,9 +3,10 @@ import React from "react";
 export const Consulting = (props) => {
     return (
         <div id="consulting">
-            <h1>
-                Coming Soon...
-            </h1>
+            
+            <div className="consulting banner">
+                <img src="" alt="" />
+            </div>
         </div>
     );
 };
