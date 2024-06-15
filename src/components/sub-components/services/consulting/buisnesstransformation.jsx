20e637cd26968d065fe23a1c375623cb350b685e
Devashish jindal <mohijindal28@gmail.com>
2024-06-15 14:42:59 +0530

    style: Adjust business banner margin and padding for better alignment

--- src/components/sub-components/services/consulting/buisnesstransformation.jsx
+++ src/components/sub-components/services/consulting/buisnesstransformation.jsx
@@ -5,47 +5,21 @@ import { Link } from 'react-router-dom';
 
 export const Buisnesstransformation = (props) => {
     return (
-    //   <div>
-    //     <h1>
-    //       Comming Soon
-    //     </h1>
-    //   </div>
-    // );
-
-    
         <div id="buisnesstransformation">
-        
-        
-            <div className="buisness-bg">
-                <div className="buisness-banner">
-                    <div className="overlay">
-                      <div className="buisness-banner-text">
-                          <h1>
-                          Strategy consulting services
-                          </h1>
-                          <p className="buisness-banner-content">Grow and transform your business by reimagining your corporate strategy and how you work.</p>
-                      </div>
-                    </div>
-                  </div>
-                    
-            </div>
-        
-
-            {/* <h2 className="display-1 " id="b2">Transform Your Buisness with our IT Consulting</h2>
-            <h1 className="display-1" id="headding">Maximize Efficency and Innovation </h1>
-
-            <div className="sec1buttons">
-            <div className="getStartedB">
-            <button type="button" class="btn btn-lg" id="buisnessButton" href="#features">
-              <a href="#features">Get Started</a> 
-            </button>
-            </div>
-            <div className="learnMoreB">
-            <button type="button" class="btn btn-lg" id="buisnessButton">Learn More</button>
-            </div>
-            </div> */}
-            <div className="content-wrapper">
-        <aside className="sidebar">
+          <div className="buisness-bg">
+            <div className="buisness-banner">
+              <div className="overlay">
+                <div className="buisness-banner-text">
+                  <h1>
+                  Strategy consulting services
+                  </h1>
+                  <p className="buisness-banner-content">Grow and transform your business by reimagining your corporate strategy and how you work.</p>
+                </div>
+              </div>
+            </div>      
+          </div>
+          <div className="content-wrapper">
+          <aside className="sidebar">
           <ul>
             <li><a href="#overview" className="page-scroll">Overview</a></li>
             <li><a href="#working" className="page-scroll">Capabilities</a></li>
